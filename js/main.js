4ec13ee1d055153dc7c225840f5f11ab7ca2881c
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-05-15 19:04:03 +0500

    add slider

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function() {
+  $('.top-slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+
+        }
+      }
+
+    ]
+  });
+});
