a52e213fe8f44abada97daa221318281bc36825a
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-05-18 23:13:44 +0500

    add slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-$(function() {
+$(function () {
   $('.top-slider').slick({
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="next"></button>',
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="prev"></button>',
     responsive: [
       {
         breakpoint: 1024,
